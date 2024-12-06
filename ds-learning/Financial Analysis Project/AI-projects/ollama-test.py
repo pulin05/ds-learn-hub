c60b13cb1fea1f450fbf6af45cc324d1bb488af4
Pulin Kulshrestha <pulinkul5@gmail.com>
2024-12-06 20:39:58 +0530

    inital commit

--- ds-learning/Financial Analysis Project/AI-projects/ollama-test.py
+++ ds-learning/Financial Analysis Project/AI-projects/ollama-test.py
@@ -1,12 +1,28 @@
-from ollama import chat
-from ollama import ChatResponse
+import requests
+import json
 
-response: ChatResponse = chat(model='llama3.2', messages=[
-  {
-    'role': 'user',
-    'content': 'Why is the sky blue?',
-  },
-])
-print(response['message']['content'])
-# or access fields directly from the response object
-print(response.message.content)
+# Define the base URL for the Ollama server
+base_url = "http://localhost:11434/api/generate"
+
+headers = {"Content-type": "application/json"}
+
+# The model you want to use (e.g., 'llama', 'alpaca')
+model = "llama3.2"
+query = "what is AI?"
+
+data = {"model": model,
+        "prompt": query,
+        "stream": False}
+# Your query
+
+
+# Sending the request to Ollama
+response = requests.post(base_url,headers=headers,json=data)
+# Check if the request was successful
+if response.status_code == 200:
+    data = response.json()['response']
+    print("Response from Ollama:")
+    print(data)
+else:
+    print(f"Failed to connect. Status code: {response.status_code}")
+    print(response.text)
---
+++ ds-learning/projects/emailinator/main.py
@@ -0,0 +1,58 @@
+import os.path
+import pathlib
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+token_file = f"{pathlib.Path(__file__).parent.resolve()}/token.json"
+
+def main():
+  """Shows basic usage of the Gmail API.
+  Lists the user's Gmail labels.
+  """
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow compleclear
+  # tes for the first
+  # time.
+  if os.path.exists(token_file):
+    creds = Credentials.from_authorized_user_file(token_file, SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          token_file, SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open(token_file, "w") as token:
+      token.write(creds.to_json())
+
+  try:
+    # Call the Gmail API
+    service = build("gmail", "v1", credentials=creds)
+    results = service.users().labels().list(userId="me").execute()
+    labels = results.get("labels", [])
+
+    if not labels:
+      print("No labels found.")
+      return
+    print("Labels:")
+    for label in labels:
+      print(label["name"])
+
+  except HttpError as error:
+    # TODO(developer) - Handle errors from gmail API.
+    print(f"An error occurred: {error}")
+
+
+if __name__ == "__main__":
+  main()
+# [END gmail_quickstart]
